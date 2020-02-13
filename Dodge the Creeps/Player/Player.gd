3769255969a5fd518a7e8be4390f83b47b63f839
Chris McAulay <cmcaulay07@gmail.com>
2020-02-13 15:52:40 -0700

    UI Design

--- Dodge the Creeps/Player/Player.gd
+++ Dodge the Creeps/Player/Player.gd
@@ -5,22 +5,24 @@ signal hit
 export var speed = 400
 var screen_size
 
+var target = Vector2()
+
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
 	
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
+	
 func _process(delta):
 	var velocity = Vector2()
 	
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-		
+	if position.distance_to(target) > 10:
+		velocity = (target - position).normalized() * speed
+	else:
+		velocity = Vector2()
+	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
@@ -47,6 +49,7 @@ func _on_Player_body_entered(body):
 	
 func start(pos):
 	position = pos
+	target = pos
 	show()
 	$CollisionShape2D.disabled = false
 
